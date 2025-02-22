39a055606bed4644d5fc0486584e2c5acdf8b2a8
yeesem <yeesemoon2002@gmail.com>
2025-02-22 14:36:53 +0800

    commit

--- simple_agent_langgraph.py
+++ simple_agent_langgraph.py
@@ -6,31 +6,40 @@ from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, END
 
 from langchain_ollama import ChatOllama
+from langchain_community.tools.tavily_search import TavilySearchResults
 
-model = ChatOllama(model = "deepseek-r1:8b")
+load_dotenv()
+os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
 
-class State(TypedDict):
-    messages : Annotated[list, add_messages]
+model = ChatOllama(model = "llama3.1")
+tool = TavilySearchResults(max_results = 2)
+tools = [tool]
+
+model_with_tools = model.bind_tools(tools)
+print(model_with_tools.invoke("What is the current stock price of tenaga?"))
+
+# class State(TypedDict):
+#     messages : Annotated[list, add_messages]
     
-def bot(state: State):
-    return {"messages" : [model.invoke(state["messages"])]}
+# def bot(state: State):
+#     return {"messages" : [model_with_tools.invoke(state["messages"])]}
 
-graph_builder = StateGraph(State)
-graph_builder.add_node("bot", bot)
+# graph_builder = StateGraph(State)
+# graph_builder.add_node("bot", bot)
 
-graph_builder.set_entry_point("bot")
-graph_builder.set_finish_point("bot")
+# graph_builder.set_entry_point("bot")
+# graph_builder.set_finish_point("bot")
 
-graph = graph_builder.compile()
+# graph = graph_builder.compile()
 
-# res = graph.invoke({"messages" : ["Hello, how are you?"]})
-# print(res["messages"])
+# # res = graph.invoke({"messages" : ["Hello, how are you?"]})
+# # print(res["messages"])
 
-while True:
-    user_input = input("User : ")
-    if user_input.lower() in ["quit", "exit", "q"]:
-        print("Goodbye!")
-        break
-    for event in graph.stream({"messages" : ("user", user_input)}):
-        for value in event.values():
-            print("Assistant : ", value["messages"][-1].content)
+# while True:
+#     user_input = input("User : ")
+#     if user_input.lower() in ["quit", "exit", "q"]:
+#         print("Goodbye!")
+#         break
+#     for event in graph.stream({"messages" : ("user", user_input)}):
+#         for value in event.values():
+#             print("Assistant : ", value["messages"][-1].content)
