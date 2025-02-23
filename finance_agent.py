c624061b6c0c55ede4ad3aa4297e4269358c3d5d
yeesem <yeesemoon2002@gmail.com>
2025-02-23 13:50:06 +0800

    commit

--- finance_agent.py
+++ finance_agent.py
@@ -61,7 +61,130 @@ FEEDBACK_PROMPT = """You are a reviewer. Provide detailed feedback and critique 
 WRITE_REPORT_PROMPT = """You are a financial report writer. Write a comprehensive financial report based on the analysis, competitor research, comparison, and feedback provided."""
 RESEARCH_CRITIQUE_PROMPT = """You are a researcher tasked with providing information to address the provided critique. Generate a list of search queries to gather relevant information. Only generate 3 queries max."""
 
+def gather_financial_node(state : AgentState):
+    # Read the CSV file into a pandas dataframe
+    csv_file = state["csv_file"]
+    df = pd.read_csv(StringIO(csv_file))
+    
+    # Convert the Dataframe to a string
+    financial_data_str = df.to_string(index = False)
+    
+    # Combine the financial data string with the task
+    combine_content = (
+        f"{state['task']}\n\n Here is the financial data: \n\n{financial_data_str}"
+    )
+    
+    messages = [
+        SystemMessage(content = GATHER_FINANCIALS_PROMPT),
+        HumanMessage(content=combine_content)
+    ]
+    
+    response = model.invoke(messages)
+    return {"financial_data" : response.content}
 
+def analyze_data_node(state : AgentState):
+    messages = [
+        SystemMessage(content=ANALYZE_DATA_PROMPT),
+        HumanMessage(content=state["financial_data"])
+    ]
+    
+    response = model.invoke(messages)
+    
+    return {"analysis" : response.content}
+
+def research_competitors_node(state : AgentState):
+    content = state["content"] or []
+    for competitor in state["competitors"]:
+        queries = model.with_structured_output(Queries).invoke(
+            [
+                SystemMessage(content=RESEARCH_COMPETITORS_PROMPT),
+                HumanMessage(content=competitor)
+            ]
+        )
+        
+        for q in queries.queries:
+            response = tavily.search(q)
+            for r in response["results"]:
+                content.append(r["content"])
+                
+        return {"content" : content}
+    
+def compare_performance_node(state : AgentState):
+    content = "\n\n".join(state["content"] or [])
+    user_message = HumanMessage(
+        content = f"{state["task"]}\n\nHere is the financial analysis:\n\n{state['analysis']}"
+    )
+    messages = [
+        SystemMessage(content=COMPETE_PERFORMANCE_PROMPT.format(content=content)),
+        user_message
+    ]
+    
+    response = model.invoke(messages)
+    
+    return {
+        "comparison" : response.content,
+        "revision_number" : state.get("revision_number", 1) + 1
+    }
+    
+def research_critique_node(state : AgentState):
+    queries = model.with_structured_output(Queries).invoke(
+        [
+            SystemMessage(content=RESEARCH_CRITIQUE_PROMPT),
+            HumanMessage(content=state["feedback"])
+        ]
+    )
+    
+    content = state["content"] or []
+    for q in queries.queries:
+        response = tavily.search(query = q, max_results = 2)
+        for r in response["resuits"]:
+            content.append(r["content"])
+        
+        return {"content" : content}
+    
+def collect_feedback_node(state : AgentState):
+    messages = [
+        SystemMessage(content = FEEDBACK_PROMPT),
+        HumanMessage(content = state["comparison"])
+    ]
+    
+    response = model.invoke(messages)
+    
+    return {"feedback" : response.content} 
+
+def write_report_node(state : AgentState):
+    messages = [
+        SystemMessage(content = WRITE_REPORT_PROMPT),
+        HumanMessage(content = state["comparison"])
+    ]
+    
+    response = model.invoke(messages)
+    
+    return {"report" : response.content}
+
+def should_continue(state : AgentState):
+    if state["revision_numnber"] > state["max_revision"]:
+        return END
+    
+    return "collect_feedback"
+
+builder = StateGraph()
+
+builder.add_node("gather_financials", gather_financial_node)
+builder.add_node("analyze_data", analyze_data_node)
+builder.add_node("research_competitors", research_competitors_node)
+builder.add_node("compare_performance", compare_performance_node)
+builder.add_node("collect_feedback", collect_feedback_node)
+builder.add_node("research_critique", research_critique_node)
+
+builder.add_node("write_report", write_report_node)
+
+builder.set_entry_point("gather_financials")
+
+    
+
+
+    
     
 
 
